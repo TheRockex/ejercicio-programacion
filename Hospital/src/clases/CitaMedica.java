3d1e5514f0ffeb73f81279e4517218824f362f55
JohanLeoBenZam <yojan.benzam@gmail.com>
2023-11-17 13:42:15 +0100

    Paciente y CitaMedica class creado

---
+++ Hospital/src/clases/CitaMedica.java
@@ -0,0 +1,57 @@
+package clases;
+
+public class CitaMedica {
+	private String centro;
+	private String especialidad;
+	private String doctor;
+	private String fecha;
+	private String hora;
+	
+	public CitaMedica(String centro, String especialidad, String doctor, String fecha, String hora) {
+		this.centro = centro;
+		this.especialidad = especialidad;
+		this.doctor = doctor;
+		this.fecha = fecha;
+		this.hora = hora;
+	}
+
+	public String getCentro() {
+		return centro;
+	}
+
+	public void setCentro(String centro) {
+		this.centro = centro;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	public String getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(String doctor) {
+		this.doctor = doctor;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+}
---
+++ Hospital/src/clases/Paciente.java
@@ -0,0 +1,70 @@
+package clases;
+
+import java.util.ArrayList;
+
+public class Paciente {
+	private int id;
+	private String nombre;
+	private String[] apellidos;
+	private String naicimento;
+	private String localidad;
+	private ArrayList<CitaMedica> listaCitas;
+	
+	public Paciente(int id, String nombre, String[] apellidos, String naicimento, String localidad,
+			ArrayList<CitaMedica> listaCitas) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.naicimento = naicimento;
+		this.localidad = localidad;
+		this.listaCitas = listaCitas;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String[] getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String[] apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getNaicimento() {
+		return naicimento;
+	}
+
+	public void setNaicimento(String naicimento) {
+		this.naicimento = naicimento;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public ArrayList<CitaMedica> getListaCitas() {
+		return listaCitas;
+	}
+
+	public void setListaCitas(ArrayList<CitaMedica> listaCitas) {
+		this.listaCitas = listaCitas;
+	}
+}
